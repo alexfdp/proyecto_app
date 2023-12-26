10d31ba539126750901abc9b8bde55db294552ed
Alex Fajardo <alexf.124@hotmail.com>
2023-12-25 22:36:38 -0500

    commit 2

---
+++ app/src/main/java/com/example/proyecto_app/single/TemporalActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_app.single;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto_app.R;
+
+public class TemporalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temporal);
+    }
+}
