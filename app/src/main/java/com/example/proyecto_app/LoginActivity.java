bc6c8aa7c0253323eff00964ecaf85132900fd76
Alex Fajardo <alexf.124@hotmail.com>
2024-01-07 21:15:57 -0500

    commit 4

--- app/src/main/java/com/example/proyecto_app/LoginActivity.java
+++ app/src/main/java/com/example/proyecto_app/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.proyecto_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        int nightModeFlags = this.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {/* si esta activo el modo oscuro, lo desactiva */
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
